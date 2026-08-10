a31dc8b1a340004326921a4c60655b446ac3387e
Sayma Shaikh <158597290+Sksayma047@users.noreply.github.com>
2026-08-10 20:58:34 +0530

    Update apiUrl to new production URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://library-management-system-lake-chi.vercel.app'
+  apiURL: 'https://vercel.com/sayma-shaikhs-projects/library-management-system'
 };
