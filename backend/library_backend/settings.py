e676096e48198402136fa514fabc15e0dfa8c7e3
Sayma Shaikh <158597290+Sksayma047@users.noreply.github.com>
2026-08-10 18:07:54 +0530

    Update settings.py

--- backend/library_backend/settings.py
+++ backend/library_backend/settings.py
@@ -60,7 +60,7 @@ DATABASES = {
         'USER': config('DB_USER', default='postgres'),
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5433'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
