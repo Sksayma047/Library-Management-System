0edd15816656b4f63defd473d49defbdfb297b82
Sayma Shaikh <158597290+Sksayma047@users.noreply.github.com>
2026-08-11 12:34:02 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://library-management-system-kuss.vercel.app/api'
+  apiURL: 'https://library-management-system-lake-chi.vercel.app/api/'
 };
